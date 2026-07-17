c2491dc0285c1eb53268b628684fa2e1877c8f4d
Eshkereshek <eshkereshek@gmail.com>
2026-07-17 12:46:52 +0300

    Update DownloadPage.tsx

--- src/DownloadPage.tsx
+++ src/DownloadPage.tsx
@@ -28,7 +28,7 @@ function DownloadPage() {
               Pagrysha Launcher просто лучше.
             </p>
             
-            <a href="https://github.com/eshkereshek/pg_launcher/releases/latest/download/Pagrysha.Launcher.7.0.3.exe" className="download-btn-green" target="_blank" rel="noreferrer">
+            <a href="https://github.com/eshkereshek/pg_launcher/releases/latest/download/Pagrysha.Launcher.7.0.4.exe" className="download-btn-green" target="_blank" rel="noreferrer">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{marginRight: '10px'}}><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
               Скачать для Windows
             </a>
