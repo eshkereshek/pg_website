d123e3b24effd445146562a7efe40b31836cd632
Eshkereshek <eshkereshek@gmail.com>
2026-07-17 12:43:24 +0300

    Update DownloadPage.tsx

--- src/DownloadPage.tsx
+++ src/DownloadPage.tsx
@@ -55,7 +55,7 @@ function DownloadPage() {
               <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M0 3.449L9.75 2.1v9.451H0m10.949-9.602L24 0v11.4H10.949M0 12.6h9.75v9.451L0 20.699M10.949 12.6H24V24l-13.051-1.801"/></svg>
               WINDOWS
             </div>
-            <a href="https://github.com/eshkereshek/pg_launcher/releases/latest/download/Pagrysha.Launcher.7.0.3.exe" className="version-btn" target="_blank" rel="noreferrer">
+            <a href="https://github.com/eshkereshek/pg_launcher/releases/latest/download/Pagrysha.Launcher.7.0.4.exe" className="version-btn" target="_blank" rel="noreferrer">
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
               Скачать
             </a>
